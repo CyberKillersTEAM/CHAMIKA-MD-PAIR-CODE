376e1a861198e280641dc088d1a88a6a10fb3d55
CʏʙᴇʀKɪʟʟᴇʀꜱ Tᴇᴀᴍ 👽 <161101124+CyberKillersTEAM@users.noreply.github.com>
2024-02-29 11:18:24 +0530

    CHAMIKA-MD WHATSAPP USER BOT 🤠

--- index.js
+++ index.js
@@ -1,120 +1,258 @@
-//copy code? pls put my github name as credit
-//🖕 to those who copy without credit
-const makeWASocket = require("@whiskeysockets/baileys").default
-const qrcode = require("qrcode-terminal")
-const fs = require('fs')
-const pino = require('pino')
-const { delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys")
-const Pino = require("pino")
-const NodeCache = require("node-cache")
-const chalk = require("chalk")
-const readline = require("readline")
-const { parsePhoneNumber } = require("libphonenumber-js")
+//code owner - XeonBotInc
+try {
+  const makeWASocket = require("@whiskeysockets/baileys").default;
+  const qrcode = require("qrcode-terminal");
+  const fs = require("fs");
+  const pino = require("pino");
+  const {
+    delay,
+    useMultiFileAuthState,
+    BufferJSON,
+    fetchLatestBaileysVersion,
+    PHONENUMBER_MCC,
+    DisconnectReason,
+    makeInMemoryStore,
+    jidNormalizedUser,
+    makeCacheableSignalKeyStore,
+    Browsers,
+  } = require("@whiskeysockets/baileys");
+  const Pino = require("pino");
+  const NodeCache = require("node-cache");
+  const chalk = require("chalk");
+  const readline = require("readline");
+  const { parsePhoneNumber } = require("libphonenumber-js");
 
+  const pairingCode = true;
+  const useMobile = process.argv.includes("--mobile");
 
-let phoneNumber = "916909137213"
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const question = (text) =>
+    new Promise((resolve) => rl.question(text, resolve));
+  async function start() {
+    let phoneNumber = await question(
+      chalk.bgBlack(chalk.greenBright(`🚀 Enter your mobile-number:`)),
+    );
 
-const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
-const useMobile = process.argv.includes("--mobile")
+    async function qr(phoneNumber) {
+      //------------------------------------------------------
+      const dirsession = `./sessions${phoneNumber}`;
+      if (!fs.existsSync(dirsession)) {
+        fs.mkdirSync(dirsession);
+      }
+      let { version, isLatest } = await fetchLatestBaileysVersion();
+      const { state, saveCreds } = await useMultiFileAuthState(dirsession);
+      const msgRetryCounterCache = new NodeCache(); // for retry message, "waiting message"
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-const question = (text) => new Promise((resolve) => rl.question(text, resolve))
+      const browsers = ["Safari", "Firefox", "Chrome", "EDGE"];
 
-
-  async function qr() {
-//------------------------------------------------------
-let { version, isLatest } = await fetchLatestBaileysVersion()
-const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
-    const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
-    const XeonBotInc = makeWASocket({
-        logger: pino({ level: 'silent' }),
-        printQRInTerminal: !pairingCode, // popping up QR in terminal log
-      mobile: useMobile, // mobile api (prone to bans)
-      browser: ['Chrome (Linux)', '', ''], // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
-     auth: {
-         creds: state.creds,
-         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
-      },
-      browser: ['Chrome (Linux)', '', ''], // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
-      markOnlineOnConnect: true, // set false for offline
-      generateHighQualityLinkPreview: true, // make high preview link
-      getMessage: async (key) => {
-         let jid = jidNormalizedUser(key.remoteJid)
-         let msg = await store.loadMessage(jid, key.id)
-
-         return msg?.message || ""
-      },
-      msgRetryCounterCache, // Resolve waiting messages
-      defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
-   })
-
-
-    // login use pairing code
-   // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
-   if (pairingCode && !XeonBotInc.authState.creds.registered) {
-      if (useMobile) throw new Error('Cannot use pairing code with mobile api')
-
-      let phoneNumber
-      if (!!phoneNumber) {
-         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-
-         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
-            process.exit(0)
-         }
-      } else {
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +916909137213 : `)))
-         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-
-         // Ask again when entering the wrong number
-         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
-
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +916909137213 : `)))
-            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-            rl.close()
-         }
+      function getRandomBrowser() {
+        const randomIndex = Math.floor(Math.random() * browsers.length);
+        return browsers[randomIndex];
       }
 
-      setTimeout(async () => {
-         let code = await XeonBotInc.requestPairingCode(phoneNumber)
-         code = code?.match(/.{1,4}/g)?.join("-") || code
-         console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
-      }, 3000)
-   }
-//------------------------------------------------------
-    XeonBotInc.ev.on("connection.update",async  (s) => {
-        const { connection, lastDisconnect } = s
+      const randomBrowser = getRandomBrowser();
+
+      const latestWebVersion = () => {
+        let version;
+        try {
+          let a = fetchJson(
+            "https://web.whatsapp.com/check-update?version=1&platform=web",
+          );
+          version = [a.currentVersion.replace(/[.]/g, ", ")];
+        } catch {
+          version = [2, 2204, 13];
+        }
+        return version;
+      };
+      const XeonBotInc = makeWASocket({
+        logger: pino({ level: "silent" }),
+        printQRInTerminal: !pairingCode, // popping up QR in terminal log
+        mobile: useMobile, // mobile api (prone to bans)
+        auth: {
+          creds: state.creds,
+          keys: makeCacheableSignalKeyStore(
+            state.keys,
+            Pino({ level: "fatal" }).child({ level: "fatal" }),
+          ),
+        },
+        browser: Browsers.macOS(randomBrowser), // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
+        markOnlineOnConnect: false, // set false for offline
+        generateHighQualityLinkPreview: true, // make high preview link
+        getMessage: async (key) => {
+          let jid = jidNormalizedUser(key.remoteJid);
+          let msg = await store.loadMessage(jid, key.id);
+
+          return msg?.message || "";
+        },
+        msgRetryCounterCache, // Resolve waiting messages
+        defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
+        latestWebVersion,
+      });
+
+      // login use pairing code
+      // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
+      if (pairingCode && !XeonBotInc.authState.creds.registered) {
+        if (useMobile)
+          throw new Error("Cannot use pairing code with mobile api");
+
+        if (!!phoneNumber) {
+          phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
+
+          if (
+            !Object.keys(PHONENUMBER_MCC).some((v) => phoneNumber.startsWith(v))
+          ) {
+            console.log(
+              chalk.bgBlack(
+                chalk.redBright(
+                  "Start with country code of your WhatsApp Number, Example : +94766632281",
+                ),
+              ),
+            );
+            process.exit(0);
+          }
+        } else {
+          phoneNumber = await question(
+            chalk.bgBlack(chalk.greenBright(`🚀 Enter your mobile-number:`)),
+          );
+          phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
+
+          // Ask again when entering the wrong number
+          if (
+            !Object.keys(PHONENUMBER_MCC).some((v) => phoneNumber.startsWith(v))
+          ) {
+            console.log(
+              chalk.bgBlack(
+                chalk.redBright(
+                  "Start with country code of your WhatsApp Number, Example : +94766632281",
+                ),
+              ),
+            );
+
+            phoneNumber = await question(
+              chalk.bgBlack(chalk.greenBright(`🚀 Enter your phoneNumber:`)),
+            );
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, "");
+            rl.close();
+          }
+        }
+
+        setTimeout(async () => {
+          let code = await XeonBotInc.requestPairingCode(phoneNumber);
+          code = code?.match(/.{1,4}/g)?.join("-") || code;
+          console.log(
+            chalk.black(chalk.bgGreen(`🎉 Your Pairing Code : `)),
+            chalk.black(chalk.white(code)),
+          );
+        }, 3000);
+      }
+      //------------------------------------------------------
+      XeonBotInc.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect } = s;
         if (connection == "open") {
-            await delay(1000 * 10)
-            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `🪀Support/Contact Developer\n\n\n⎆Donate: https://i.ibb.co/w46VQ8D/Picsart-22-10-08-06-46-30-674.jpg\n\n⎆YouTube: https://youtube.com/@DGXeon\n\n⎆Telegram Channel: https://t.me/xeonbotinc\n\n⎆Telegram Chat: https://t.me/+AYOyJflnt-AzNGFl\n\n⎆WhatsApp Gc1: https://chat.whatsapp.com/Kjm8rnDFcpb04gQNSTbW2d\n\n⎆WhatsApp Gc2: https://chat.whatsapp.com/EEOnU0V7dl9HF1mMFO8QWa\n\n⎆WhatsApp Gc3: https://chat.whatsapp.com/Dh0lD0Ee5hN1JMFXNqtxSG\n\n⎆WhatsApp Pm: Wa.me/916909137213\n\n⎆Instagram: https://instagram.com/unicorn_xeon13\n\n⎆GitHub: https://github.com/DGXeon/\n\n⎆Blog: https://dreamguyxeonfiles.blogspot.com/2022/05/bots%20whatsapp%20mods.html?m=1\n\n\n` });
-            let sessionXeon = fs.readFileSync('./sessions/creds.json');
-            await delay(1000 * 2) 
-             const xeonses = await  XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` })
-             await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: ` *ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ✅* ` }, {quoted: xeonses});
-              await delay(1000 * 2) 
-              process.exit(0)
+          await delay(1000 * 10);
+
+          function generateRandomText() {
+            const prefix = "3EB";
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            let randomText = prefix;
+
+            for (let i = prefix.length; i < 22; i++) {
+              const randomIndex = Math.floor(Math.random() * characters.length);
+              randomText += characters.charAt(randomIndex);
+            }
+
+            return randomText;
+          }
+
+          const randomText = generateRandomText();
+
+          let sessionXeon = fs.readFileSync(`${dirsession}/creds.json`);
+          let c = Buffer.from(sessionXeon).toString("base64");
+
+          /*let {upload} = require('./mega')
+
+const user_jid = jidNormalizedUser(XeonBotInc.user.id);
+                const mega_url = await upload(fs.createReadStream('./sessions/' + 'creds.json'), `${user_jid}.json`);
+const string_session = mega_url.replace('https://mega.nz/file/', '')
+let md = "PRABATH-MD~" + string_session
+await XeonBotInc.sendMessage(user_jid, {
+                  text: md
+             }, {messageId: randomText});*/
+          await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: c });
+
+          ///////////////////////////////////////////
+
+          try {
+            const { upload } = require("./mega");
+            const user_jid = jidNormalizedUser(XeonBotInc.user.id);
+            const mega_url = await upload(
+              fs.createReadStream(`${dirsession}/creds.json`),
+              `${user_jid}.json`,
+            );
+            const string_session = mega_url.replace(
+              "https://mega.nz/file/",
+              "",
+            );
+            let md = string_session;
+            await XeonBotInc.sendMessage(
+              user_jid,
+              { text: md },
+              { messageId: randomText },
+            );
+          } catch (e) {
+            XeonBotInc.sendMessage(
+              XeonBotInc.user.id,
+              { text: c },
+              { messageId: randomText },
+            );
+          }
+
+          let x = "```";
+          let desc = `*Welcome to CHAMIKA-MD WhatsApp Bot* 🎉`;
+
+          //await XeonBotInc.sendMessage(XeonBotInc.user.id, {text:desc}, {messageId: randomText})
+
+          await delay(1000 * 2);
+          await XeonBotInc.ws.close();
+          fs.rmSync(`${dirsession}/`, {
+            recursive: true,
+            force: true,
+          });
+
+          question(
+            chalk.bgBlack(chalk.greenBright(`✔️ Connection Successful`)),
+          );
+
+          await delay(1000 * 2);
+          process.exit(0);
         }
         if (
-            connection === "close" &&
-            lastDisconnect &&
-            lastDisconnect.error &&
-            lastDisconnect.error.output.statusCode != 401
+          connection === "close" &&
+          lastDisconnect &&
+          lastDisconnect.error &&
+          lastDisconnect.error.output.statusCode != 401
         ) {
-            qr()
+          await qr(phoneNumber);
         }
-    })
-    XeonBotInc.ev.on('creds.update', saveCreds)
-    XeonBotInc.ev.on("messages.upsert",  () => { })
+      });
+      XeonBotInc.ev.on("creds.update", saveCreds);
+      XeonBotInc.ev.on("messages.upsert", () => {});
+    }
+    await qr(phoneNumber);
+  }
+  start();
+  process.on("uncaughtException", function (err) {
+    let e = String(err);
+    if (e.includes("Socket connection timeout")) return;
+    if (e.includes("rate-overlimit")) return;
+    if (e.includes("Connection Closed")) return;
+    if (e.includes("Timed Out")) return;
+    if (e.includes("Value not found")) return;
+    console.log("Caught exception: ", err);
+  });
+} catch (e) {
+  //console.log(e)
 }
-qr()
-
-process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("Socket connection timeout")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
-})
